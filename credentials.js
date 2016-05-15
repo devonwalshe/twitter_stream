9501be575fed67fe0eda5258f7fcd40b06f407ad
Devon Walshe <devon.walshe@gmail.com>
2016-05-15 17:37:06 -0500

    removed creds

--- credentials.js
+++ credentials.js
@@ -1,7 +1,7 @@
 var credentials = {
-      consumer_key: '7bkJ9xKOfpoLCZf8FPAgYvwz4',
-      consumer_secret: 'kZRxMVVBMljYOQGhlPLUNRFG4cMrEJ5697JOM1t7jpbB0S0zaV',
-      access_token: '171676219-QUshhVuQQ9Yj9V2Id1n6fRP7bcYiVhRAdUBMb8tA',
-      access_token_secret: 'pFNLPiF9kLw94FKB4qw9639LEoAgswA4u8iT17gAbzMAY'
+      consumer_key: 'CONSUMER_KEY',
+      consumer_secret: 'CONSUMER_SECRET',
+      access_token: 'ACCESS_TOKEN',
+      access_token_secret: 'ACCESS_SECRET'
 };
 module.exports = credentials;
--- keywords.js
+++ keywords.js
@@ -1,6 +1,3 @@
-var keywords = "sauchiehall street,gorbals,glasgow council,city council,glasgow,GCC,GCMB,city centre,potholes,dog fouling,fouling,easterhouse,GEAPP,future cities,open glasgow,cycle,CSG,community safety glasgow,FOI,freedom of information";
-
-// Testing a larger number of tweets
-// var keywords = "tree,house,tree house";
+var keywords = "KEYWORDS";
 
 module.exports = keywords;
